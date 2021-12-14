138d1599bd90fa6bd7baa712a66bea06d9fb991c
Shane Barnwell <sbarnwell@macu.com>
2021-12-14 15:11:33 -0700

    test env var

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,18 @@
 import Nav from '../components/nav';
 
-export default function About() {
+export default function About(props) {
     return (
         <div>
             <Nav/>
-            <h1> About </h1>
+            <h1> About ({props.var})</h1>
         </div>
     );
+}
+
+export async function getStaticProps(context) {
+    return {
+        props: {
+            var: process.env.SERVER_VARIABLE,
+        },
+    };
 }
